292f77a63461ed21526f07deee2a4735708eb5b7
Crimsondragon2002 <91789792+Crimsondragon2002@users.noreply.github.com>
2023-10-27 10:07:42 -0400

    Update WatchCheck.java
    switched to using nanoseconds for base calculations

--- WatchCheck.java
+++ WatchCheck.java
@@ -3,11 +3,14 @@ public class WatchCheck
 	private final long beginCheck;
 	public WatchCheck()
 	{
-		beginCheck = System.currentTimeMillis();
+		beginCheck = System.nanoTime();
 	}
 	public double timing()
 	{
-		long endCheck = System.currentTimeMillis();
-		return ((endCheck-beginCheck));
+		long endCheck = System.nanoTime();
+		Instant endCheck2 = Instant.now();
+
+		return (((endCheck-beginCheck)/(1000000)));
+		
 	}
 }
